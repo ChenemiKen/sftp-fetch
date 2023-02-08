8cc0a50c8cbb4589f93a28aad69f01c6effaa33a
ChenemiKen <chenemiken15@gmail.com>
2023-02-08 07:10:21 +0100

    implemented downloads and move

--- src/main/java/com/chenemiken/ftpfetch/AppRunner.java
+++ src/main/java/com/chenemiken/ftpfetch/AppRunner.java
@@ -3,6 +3,7 @@ package com.chenemiken.ftpfetch;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
+import java.io.File;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,11 +15,20 @@ public class AppRunner implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println(host.address());
-    connect();
+    try{
+//      connect();
+//      listFiles("Documents/fol1");
+//      moveOneFile("Documents/fol1", "/courses.txt", "Documents/fol2");
+//      moveAllFiles("Documents/fol1/", "Documents/fol2/");
+//      downloadOneFile("Documents/fol2/", "courses.txt");
+      downloadAllFiles("Documents/fol2/");
+      System.exit(0);
+    }catch (Exception e){
+      System.out.println(e.getLocalizedMessage());
+    }
   }
 
-  public ChannelSftp connect() throws Exception{
+  private ChannelSftp connect() throws Exception{
     JSch jsch = new JSch();
     Session session =  jsch.getSession(host.username(), host.address(), host.port());
     session.setPassword(host.password());
@@ -30,8 +40,124 @@ public class AppRunner implements CommandLineRunner {
     session.connect();
     ChannelSftp channelSftp = (ChannelSftp) session.openChannel("sftp");
     channelSftp.connect();
-    System.out.println("sftp connected");
-    System.out.println(channelSftp.pwd());
+    System.out.println("sftp connected!");
+    System.out.println(">>>>>>>Currently in: " +channelSftp.pwd());
     return channelSftp;
+  }
+
+  public void listFiles(String sftpSourcePath) throws Exception {
+    ChannelSftp channelSftp = connect();
+    try {
+      File folder = new File(channelSftp.realpath(sftpSourcePath));
+      System.out.println(">>>>>>folder: "+folder);
+      File[] listOfFiles = folder.listFiles();
+//      System.out.println(">>>>>>>listOfFiles: "+listOfFiles);
+      for (File file : listOfFiles) {
+        String fileName = file.getName();
+        System.out.println(">>>>>fileName: "+fileName);
+      }
+      channelSftp.disconnect();
+      System.out.println("sftp disconnected");
+//      session.disconnect();
+    } finally {
+			if (channelSftp != null && channelSftp.isConnected()) {
+				channelSftp.disconnect();
+			}
+//			if (session != null && session.isConnected()) {
+//				session.disconnect();
+//			}
+    }
+  }
+
+  public void moveOneFile(String sftpSourcePath, String fileName, String destinationPath) throws Exception{
+    ChannelSftp channelSftp = connect();
+    try {
+      File file = new File(channelSftp.realpath(sftpSourcePath + fileName));
+      System.out.println(">>>>>>file: "+file);
+      if(file.exists()){
+        boolean success = file.renameTo(new File(channelSftp.realpath(destinationPath + fileName)));
+        System.out.println(success);
+      }else{
+        System.out.println(">>>>>>Error: file not found");
+      }
+    }finally {
+      if (channelSftp != null && channelSftp.isConnected()) {
+        channelSftp.disconnect();
+      }
+    }
+  }
+
+  public void moveAllFiles(String sftpSourcePath, String destinationPath) throws Exception{
+    ChannelSftp channelSftp = connect();
+    try{
+      File folder = new File(channelSftp.realpath(sftpSourcePath));
+      System.out.println(">>>>>>SourceFolder: "+folder);
+      File[] listOfFiles = folder.listFiles();
+      for (File file : listOfFiles) {
+        String fileName = file.getName();
+        File fileToMove = new File(channelSftp.realpath(sftpSourcePath + fileName));
+        File newFile = new File(channelSftp.realpath((destinationPath + fileName)));
+        boolean success = fileToMove.renameTo(newFile);
+        System.out.println(newFile.getAbsoluteFile());
+        System.out.println(success);
+      }
+    }catch (Exception e){
+      System.out.println(e.getLocalizedMessage());
+    }finally {
+      if (channelSftp != null && channelSftp.isConnected()) {
+        channelSftp.disconnect();
+      }
+    }
+  }
+
+  public void downloadOneFile(String sftpSourcePath, String fileName) throws Exception{
+    ChannelSftp channelSftp = connect();
+    try {
+      File file = new File(channelSftp.realpath(sftpSourcePath + fileName));
+      System.out.println(">>>>>>folder: "+file);
+      if(file.exists()){
+        File localDownloadDir = new File("./downloads/");
+        if(!localDownloadDir.exists()){
+          localDownloadDir.mkdir();
+        }
+        boolean success = file.renameTo(new File(localDownloadDir, fileName));
+        System.out.println(success);
+      }else{
+        System.out.println(">>>>>>>Error: File not found" );
+      }
+    }catch (Exception e){
+      System.out.println(e.getLocalizedMessage());
+    }finally {
+      if (channelSftp != null && channelSftp.isConnected()) {
+        channelSftp.disconnect();
+      }
+    }
+  }
+
+  public void downloadAllFiles(String sftpSourcePath) throws Exception{
+    ChannelSftp channelSftp = connect();
+    try{
+      File folder = new File(channelSftp.realpath(sftpSourcePath));
+      System.out.println(">>>>>>SourceFolder: "+folder);
+      File localDownloadDir = new File("./downloads/");
+      if(!localDownloadDir.exists()){
+        localDownloadDir.mkdir();
+      }
+      File[] listOfFiles = folder.listFiles();
+      for (File file : listOfFiles) {
+        String fileName = file.getName();
+        File fileToDownload = new File(channelSftp.realpath(sftpSourcePath + fileName));
+        File newFile = new File(localDownloadDir, fileName);
+        boolean success = fileToDownload.renameTo(newFile);
+        System.out.println(newFile.getAbsoluteFile());
+        System.out.println(success);
+      }
+    }catch (Exception e){
+      System.out.println(e.getLocalizedMessage());
+    }finally {
+      if (channelSftp != null && channelSftp.isConnected()) {
+        channelSftp.disconnect();
+      }
+    }
   }
 }
--- src/main/java/com/chenemiken/ftpfetch/FtpfetchApplication.java
+++ src/main/java/com/chenemiken/ftpfetch/FtpfetchApplication.java
@@ -1,15 +1,8 @@
 package com.chenemiken.ftpfetch;
 
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-import java.io.File;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SftpConfigProperties.class)
@@ -19,89 +12,4 @@ public class FtpfetchApplication{
 		SpringApplication.run(FtpfetchApplication.class, args);
 	}
 
-
-
-	public static void listFiles(String address, Integer serverPort, String username,
-			String destinationPath, String password) throws Exception {
-
-		try {
-			JSch jsch = new JSch();
-			Session session =  jsch.getSession(username, address, serverPort);
-			session.setPassword(password);
-
-			java.util.Properties config = new java.util.Properties();
-			config.put("StrictHostKeyChecking", "no");
-			session.setConfig(config);
-
-			session.connect();
-			ChannelSftp channelSftp = (ChannelSftp) session.openChannel("sftp");
-			channelSftp.connect();
-			System.out.println("sftp connected");
-			System.out.println(channelSftp.pwd());
-//			ChannelSftp channelSftp = connect();
-//			channelSftp.cd(destinationPath);
-//			log.info("this destination path " + destinationPath + " files are " + channelSftp
-//					.ls(destinationPath));
-//			System.out.println("this destination path " + destinationPath + " files are " + channelSftp
-//					.ls(destinationPath));
-			File folder = new File(channelSftp.realpath(destinationPath));
-			System.out.println(">>>>>>folder: "+folder);
-			File[] listOfFiles = folder.listFiles();
-			System.out.println(">>>>>>>listOfFiles: "+listOfFiles);
-			for (File file : listOfFiles) {
-				String fileName = file.getName();
-				System.out.println(">>>>>fileName: "+fileName);
-			}
-			channelSftp.disconnect();
-//			log.info("sftp disconnected");
-			System.out.println("sftp disconnected");
-			session.disconnect();
-		} finally {
-//			if (channelSftp != null && channelSftp.isConnected()) {
-//				channelSftp.disconnect();
-//			}
-//			if (session != null && session.isConnected()) {
-//				session.disconnect();
-//			}
-		}
-	}
-
-	public static void moveAFile(String serverAddress, Integer serverPort, String username,
-			String destinationPath, String password) throws Exception{
-		Session session = null;
-		ChannelSftp channelSftp = new ChannelSftp();
-		try {
-			JSch jsch = new JSch();
-			session = jsch.getSession(username, serverAddress, serverPort);
-			session.setPassword(password);
-
-			java.util.Properties config = new java.util.Properties();
-			config.put("StrictHostKeyChecking", "no");
-
-			session.setConfig(config);
-			session.connect();
-			channelSftp = (ChannelSftp) session.openChannel("sftp");
-			channelSftp.connect();
-//			log.info("sftp connected");
-			System.out.println("sftp connected");
-			System.out.println(channelSftp.pwd());
-			File folder = new File(channelSftp.realpath(destinationPath+"/courses.txt"));
-			System.out.println(">>>>>>folder: "+folder);
-//			boolean success = folder.renameTo(new File(channelSftp.realpath("Documents/fol2/courses.txt")));
-			File localDownloadDir = new File("./downloads");
-			System.out.println(localDownloadDir.exists());
-			if(!localDownloadDir.exists()){
-				localDownloadDir.mkdir();
-			}
-			System.out.println(localDownloadDir.exists());
-			boolean success = folder.renameTo(new File(localDownloadDir.toString(), "/courses.txt"));
-			System.out.println(success);
-//			File folder =
-//			File[] listOfFiles = folder.listFiles();
-
-
-		}finally {
-
-		}
-	}
 }
